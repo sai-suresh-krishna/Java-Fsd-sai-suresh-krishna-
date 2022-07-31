a8ffe7c004beb599067a46d3b5254a958fd909cf
chandu sai suresh krishna <saisureshkrishna.ch@gmail.com>
2022-07-31 22:42:09 +0530

    assisted

---
+++ assisted projects/src/Insertionsort/Insertionsort.java
@@ -0,0 +1,34 @@
+package Insertionsort;
+
+public class Insertionsort {
+	
+
+	    public static  void main(String[] args){
+
+	        int[] arr = {7,15,2,29,46};
+	        insertionSort(arr);
+	        for(int i=0;i<arr.length;i++){
+
+	            System.out.println(arr[i]);
+
+	        }
+	     }
+	    public static void insertionSort(int[] arr){
+
+	    int len = arr.length;
+	    for(int j=1;j<len;j++){
+	    int key = arr[j];
+	    int i=j-1;
+	    while ((i>-1) && (arr[i]>key)){
+
+	        arr[i+1]=arr[i];
+	        i--;
+	    }
+	    arr[i+1]=key;
+	         }
+
+	    }
+	}
+
+
+
